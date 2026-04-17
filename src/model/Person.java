a073a32470204f00de7d6fed09ba7f761d7b0642
mar1amm09 <mariammabdelhamidd@gmail.com>
2026-04-17 18:30:52 +0200

    Change Person class to abstract

--- src/model/Person.java
+++ src/model/Person.java
@@ -2,7 +2,7 @@ package model;
 
 import java.util.Date;
 
-public class Person {
+public abstract class Person {
     protected String username;
     protected String password;
     protected Date dateOfBirth;
