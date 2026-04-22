78d854dca549531f210c099641d26b99e61682dd
mariamadly006-ctrl <mariam.adly006@gmail.com>
2026-04-22 15:20:24 +0200

    Add ReservationType to Reservation class

--- HotelReservationSystem/model/Reservation.java
+++ HotelReservationSystem/model/Reservation.java
@@ -1,5 +1,6 @@
 package HotelReservationSystem.model;
 
+import HotelReservationSystem.enums.ReservationType;
 import java.util.Date;
 
 public class Reservation {
@@ -7,6 +8,7 @@ public class Reservation {
     private Room room;
     private Date checkIn;
     private Date checkOut;
+    private ReservationType type;
     public enum Status{
         PENDING,
         CONFIRMED,
@@ -15,12 +17,13 @@ public class Reservation {
     }
     private Status status;
     //constructor
-    public Reservation(Guest guest, Room room, Date checkIn, Date checkOut, Status status){
+    public Reservation(Guest guest, Room room, Date checkIn, Date checkOut, Status status, ReservationType type){
         this.guest = guest;
         this.room = room;
         this.checkIn = checkIn;
         this.checkOut = checkOut;
         this.status = status;
+        this.type = type;
     }
     //getters
     public Guest getGuest(){
@@ -38,6 +41,9 @@ public class Reservation {
     public Status getStatus(){
         return status;
     }
+    public ReservationType getType() {
+        return type;
+    }
     //setter
     public void setStatus(Status status){
         this.status = status;
@@ -50,5 +56,8 @@ public class Reservation {
     }
     public void setCheckOut(Date checkOut){
         this.checkOut = checkOut;
+    }
+    public void setType(ReservationType type) {
+        this.type = type;
     }
 }
