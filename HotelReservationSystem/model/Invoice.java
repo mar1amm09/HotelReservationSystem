d0629a4c9b36398c65740f681fd53ca4cb904866
mariamadly006-ctrl <mariam.adly006@gmail.com>
2026-04-22 15:25:09 +0200

    Enhance Invoice class to support reservation types
    Added ReservationType to Invoice constructor and adjusted totalAmount calculation based on reservation type.

--- HotelReservationSystem/model/Invoice.java
+++ HotelReservationSystem/model/Invoice.java
@@ -1,4 +1,5 @@
 package HotelReservationSystem.model;
+import HotelReservationSystem.enums.ReservationType;
 import HotelReservationSystem.enums.paymentMethod;
 import java.util.Date;
 import.java.util.List;
@@ -7,11 +8,19 @@ public class Invoice {
     protected List<paymentMethod>
     paymentMethods;
     private Date paymentDate;
-    public Invoice(double totalAmount,<List<paymentMethod>paymentMethos,Date paymentDate)
+    public Invoice(double totalAmount,<List<paymentMethod>paymentMethos,Date paymentDate,ReservationType type)
     {
         this.totalAmount+totalAmount;
         this.paymentMethods=paymentMethods;
         this.paymentDate=paymentDate;
+        this.type= type;
+        if (type ==ReservationType.ALL_INCLUSIVE){
+            this.totalAmount = totalAmount*1.15;}
+        else if (type == ReservationType.FULL_BOARD){
+            this.totalAmount = totalAmount*1.10;}
+        else if (type == ReservationType.HALF_BOARD){
+            this.totalAmount = totalAmount*1.05;}
+        else {this.totalAmount = totalAmount;}
     }
     //getters
     public double getTotalAmount(){
